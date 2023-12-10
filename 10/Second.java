128b408abb5caa6e4ca3c3f43e6829fb30daaef8
D3crvpt3d <d3crvpt3d@gmail.com>
2023-12-10 12:24:04 +0100

    save if it crashes before i ended 2

--- 10/Second.java
+++ 10/Second.java
@@ -6,14 +6,17 @@ class Vector {
     int x;
     int y;
     int[] prev = new int[2];
-    String direction;
+    String[][] direction;
+    int[][] lut; // 1 = pipe, 2 = right, 0 = left
 
-    public Vector(int x, int y, char[][] matrix){
+    public Vector(int x, int y, char[][] matrix, int[][] lut, String[][] direction){
         this.x = x;
         this.y = y;
         this.prev[0] = -1;
         this.prev[1] = -1;
         this.matrix = matrix;
+        this.lut = lut;
+        this.direction = direction;
     }
 
     //biased
@@ -31,27 +34,6 @@ class Vector {
 
     }
 
-    private void updateDirection(String c){
-        switch (c) {
-            case "north":
-                
-                break;
-            case "east":
-            
-                break;
-            case "south":
-            
-                break;
-            case "west":
-            
-                break;
-                
-            default:
-                System.err.println("Direction not defined:"+direction);
-                break;
-        }
-    }
-
     //changed to print direction of outside or inside
     public void takeNextStep(){
 
@@ -60,85 +42,97 @@ class Vector {
 
             case '|':
                 if(this.prev[0] == this.x && this.prev[1] == this.y-1){
+                    direction[this.x][this.y] = "↓";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.y++;
-                    updateDirection("south");
+                    
                 }else{
+                    direction[this.x][this.y] = "↑";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.y--;
-                    updateDirection("north");
+                    
                 }
 
                 break;
             case '-':
                 if(this.prev[0] == this.x-1 && this.prev[1] == this.y){
+                    direction[this.x][this.y] = "→";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.x++;
-                    updateDirection("east");
+                    
                 }else{
+                    direction[this.x][this.y] = "←";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.x--;
-                    updateDirection("west");
+                    
                 }
                 
                 break;
             case 'L':
                 if(this.prev[0] == this.x && this.prev[1] == this.y-1){
+                    direction[x][y] = "→";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.x++;
-                    updateDirection("east");
+                    
                 }else{
+                    direction[this.x][this.y] = "↑";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.y--;
-                    updateDirection("north");
+                    
                 }
 
                 break;
             case 'J':
                 if(this.prev[0] == this.x && this.prev[1] == this.y-1){
+                    direction[this.x][this.y] = "←";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.x--;
-                    updateDirection("west");
+                    
                 }else{
+                    direction[this.x][this.y] = "↑";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.y--;
-                    updateDirection("north");
+                    
                 }
                 
                 break;
             case '7':
                 if(this.prev[0] == this.x && this.prev[1] == this.y+1){
+                    direction[this.x][this.y] = "←";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.x--;
-                    updateDirection("west");
+                    
                 }else{
+                    direction[this.x][this.y] = "↓";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.y++;
-                    updateDirection("south");
+                    
                 }
                 
                 break;
             case 'F':
                 if(this.prev[0] == this.x && this.prev[1] == this.y+1){
+                    direction[this.x][this.y] = "→";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.x++;
-                    updateDirection("east");
+                    
                 }else{
+                    direction[this.x][this.y] = "↓";
                     this.prev[0] = this.x;
                     this.prev[1] = this.y;
                     this.y++;
-                    updateDirection("south");
+                    
                 }
 
                 break;
@@ -148,18 +142,96 @@ class Vector {
                 return;
         }
 
-        //print for fill algorithmus
-        System.out.println(this.prev[0]+" "+this.prev[1]+" "+this.direction);
-
 
     }
     
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class Second extends Thread{
 
-    static char[][] matrix = new char[140][140];
+    static int[][] lut = new int[140][140]; //pipe lut
+
+    static char[][] matrix = new char[140][140]; //input as matrix
+
+    static String[][] direction = new String[140][140]; //direction array
+
+    //right -> 2 left -> 3
+    private static void updateDirection(String s, int x, int y){
+        int tile_x = 0;
+        int tile_y = 0;
+
+        switch (s) {
+            case "↑":
+                tile_x = Math.min(139, x+1);
+                tile_y = y;
+                while (lut[tile_x][tile_y] != 1) {
+                    lut[tile_x][tile_x] = 2;
+                    tile_x++;
+                    if(tile_x == lut.length){break;}
+                }
+                break;
+            case "↓":
+                tile_x = Math.max(0, x-1);
+                tile_y = y;
+                while (lut[tile_x][tile_y] != 1) {
+                    lut[tile_x][tile_x] = 2;
+                    tile_x--;
+                    if(tile_x < 0){break;}
+                }
+                break;
+            case "→":
+                tile_x = x;
+                tile_y = Math.min(139, y+1);
+                while (lut[tile_x][tile_y] != 1) {
+                    lut[tile_x][tile_x] = 2;
+                    tile_y++;
+                    if(tile_y == lut.length){break;}
+                }
+                break;
+            case "←":
+                tile_x = x;
+                tile_y = Math.max(0, y-1);
+                while (lut[tile_x][tile_y] != 1) {
+                    lut[tile_x][tile_x] = 2;
+                    tile_y--;
+                    if(tile_y < 0){break;}
+                }
+                break;
+            
+            default:
+                break;
+            }
+    }
  
+
+
+/*
+ * ███▄ ▄███▓ ▄▄▄       ██▓ ███▄    █ 
+ ▓██▒▀█▀ ██▒▒████▄    ▓██▒ ██ ▀█   █ 
+ ▓██    ▓██░▒██  ▀█▄  ▒██▒▓██  ▀█ ██▒
+ ▒██    ▒██ ░██▄▄▄▄██ ░██░▓██▒  ▐▌██▒
+ ▒██▒   ░██▒ ▓█   ▓██▒░██░▒██░   ▓██░
+ ░ ▒░   ░  ░ ▒▒   ▓▒█░░▓  ░ ▒░   ▒ ▒ 
+ ░  ░      ░  ▒   ▒▒ ░ ▒ ░░ ░░   ░ ▒░
+ ░      ░     ░   ▒    ▒ ░   ░   ░ ░ 
+        ░         ░  ░ ░           ░ 
+ */
+                                      
+    
     public static void main(String[] args) throws InterruptedException {
         
         Scanner scanner = new Scanner(System.in);
@@ -174,47 +246,59 @@ public class Second extends Thread{
 
         Vector start = getS();
 
-        getMaxDistanceFromAnimal(start);
+        getMaxDistanceFromAnimal(start); // fill lut with pipes
+
+        //DEBUG
+        //print direction array
+        for (int y = 0; y < direction.length; y++) {
+            for (int x = 0; x < direction[0].length; x++) {
+                System.err.print(direction[x][y]);
+            }
+            System.err.println();
+        }
+        //DEBUG
+
+        //fill lut by looking up direction array
+        for (int y = 0; y < args.length; y++) {
+            for (int x = 0; x < args.length; x++) {
+                if(lut[x][y] == 1){
+                    updateDirection(direction[x][y], x, y);
+                }
+            }
+        }
+
+
+        for (int y = 0; y < lut.length; y++) {
+            for (int x = 0; x < lut[0].length; x++) {
+                System.out.print(lut[x][y]);
+            }
+            System.out.println();
+        }
 
     }
 
-    private static int getMaxDistanceFromAnimal(Vector start) throws InterruptedException{
 
-        int length = 0;
 
-        Vector pos1 = new Vector(start.x, start.y, matrix);
-        Vector pos2 = new Vector(start.x, start.y, matrix);
+
+    private static void getMaxDistanceFromAnimal(Vector start) throws InterruptedException{
+
+        Vector pos1 = new Vector(start.x, start.y, matrix, lut, direction);
 
         //first Step
         pos1.takeFirstStep();
 
-        pos2.prev[0] = pos1.x; // block segment of pos 1 for pos2
-        pos2.prev[1] = pos1.y;
-
-        pos2.takeFirstStep();
-
-        length++;
+        lut[pos1.x][pos1.y] = 1;
 
         //first Step end
 
-        while (!(pos1.x == pos2.x && pos1.y == pos2.y)) {
+        //128 88 start
+        
+        while (!(pos1.x == 88 && pos1.y == 128)) {
 
             //Thread.sleep(100);
-            
-           
             pos1.takeNextStep();
-
-            if(pos1.x == pos2.x && pos1.y == pos2.y){
-                break;
-            }
-
-            pos2.takeNextStep();
-        
-            length++;
+            lut[pos1.x][pos1.y] = 1;
         }
-
-
-        return length;
 
     }
 
@@ -224,7 +308,7 @@ public class Second extends Thread{
             
             for (int x = 0; x < matrix[0].length; x++) {
                 if(matrix[y][x] == 'S'){
-                    return new Vector(x, y, matrix);
+                    return new Vector(x, y, matrix, lut, direction);
                 }
             }
 
